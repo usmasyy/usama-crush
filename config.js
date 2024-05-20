e6dcf66df5061f554a569fa55b73cea00bf0db7c
usmasyy <usamahsnnn@gmail.com>
2024-05-20 21:55:04 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "usama",
+    introTitle: 'i have a crush on you',
+    introDesc: `“And I’d choose you; in a hundred lifetimes, in a hundred worlds, in any version of reality, I’d find you and I’d choose you.” — Kiersten White, from The Chaos of Stars1.`,
+    btnIntro: 'continue',
+    title: 'should we go on a date',
+    desc: ' dont say NO,actually u cant try if u want ',
+    btnYes: 'yes',
+    btnNo: 'no',
+    question: 'if you seeing this u probaly said yes, i wanna know why?',
+    btnReply: 'i love you and send',
+    reply: 'so happy for you, you are lucky i choose u with that face',
+    mess: 'you pick time',
+    messDesc: 'i will pick place lets go on a date.',
+    btnAccept: 'yes usama lets go muaah <3',
+    messLink: '' 
+}
